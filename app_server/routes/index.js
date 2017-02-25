f2320cc560db5e63fffac1868fbad8e07917e7c7
Shasankar <shasankar.paul@gmail.com>
2017-02-25 19:55:26 +0530

    before leaving the venue

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var ctrlMain = require('../controllers/main');
+var ctrlGetCar = require('../controllers/getCar');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', ctrlMain.index);
+
+getCarController = function(req, res, next) {
+  console.log(req.query.lat);
+  console.log(req.query.long);
+  res.render('car', { title: 'Get A Car', carId: 1234, time: '5 mins'});
+};
+router.get('/getCar', getCarController);
 
 module.exports = router;
